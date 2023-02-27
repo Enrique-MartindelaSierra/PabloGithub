c6e438115b0eace33f4cc14c463dc127d123dd78
Pablo Garramone <pablo@iessanvicente.com>
2023-02-27 13:32:15 +0100

    ProyctoJS v2

--- JS/proyectoJS/proyectojs.js
+++ JS/proyectoJS/proyectojs.js
@@ -19,34 +19,61 @@
 		document.body.insertBefore(lista,formulario);
 	}
 	
-
-	
-	
-	
-	
-	
 	
 	function hacerParrafos(t,f){
-		
+		let contenedor=document.createElement("div");
+		contenedor.style.border="1px solid black";
+		let elemento,texto;
+		for(let i=1;i<=f;i++){
+			elemento=document.createElement("p");
+			texto=document.createTextNode(t+"x"+i+"="+(t*i));
+			elemento.appendChild(texto);
+			contenedor.appendChild(elemento);
+		}
+		let formulario=document.formu;
+		document.body.insertBefore(contenedor,formulario);		
 		
 	}	
 	function hacerDesplegable(t,f){
-		
+		let despl=document.createElement("select");
+		let elemento,texto;
+		for(let i=1;i<=f;i++){
+			elemento=document.createElement("option");
+			elemento.value="opcion"+i;
+			texto=document.createTextNode(t+"x"+i+"="+(t*i));
+			elemento.appendChild(texto);
+			despl.appendChild(elemento);
+		}
+		let formulario=document.formu;
+		document.body.insertBefore(despl,formulario);		
 		
 	}	
+	
+	function hacerCelda(row,cadena){
+		let celda,texto;
+		celda=document.createElement("td");
+		celda.style.border="1px solid black";
+		texto=document.createTextNode(cadena);
+		celda.appendChild(texto);
+		row.appendChild(celda);					
+	}	
 	function hacerTabla(menor,mayor){
-		document.write("<table style='border:1px solid black; border-collapse:collapse;'>");
-		for(let i=1;i<=mayor;i++)
-			{
-				document.write("<tr>");
-				document.write("<td style='border:1px solid black;'>"+menor+"x"+i+"</td>");
-				document.write("<td style='border:1px solid black;'>=</td>")>
-				document.write("<td style='border:1px solid black;'>"+(menor*i)+"</td>");
-				document.write("</tr>");
-				
-			}
-		document.write("</table>");			
+		let tabla=document.createElement("table");
+		tabla.style.border="1px solid black";
+		tabla.style.borderCollapse="collapse";
+		let fila;
 		
+		for(let i=1;i<=mayor;i++)
+		{
+			fila=document.createElement("tr");
+			
+			hacerCelda(fila,menor+"x"+i);
+			hacerCelda(fila,"=");
+			hacerCelda(fila,menor*i);
+			tabla.appendChild(fila);
+		}
+		let formulario=document.formu;
+		document.body.insertBefore(tabla,formulario);	
 	}
 	
 	function menu(){
@@ -81,13 +108,40 @@
 	}
 
 	function validar(){
+		let f=document.formu;
+		if(f.tabla.value==""){
+			alert("Error el campo de la tabla debe rellenarse");
+			return false;
+		}
+		if(f.cantidad.value==""){
+			alert("Error el campo de la cantidad debe rellenarse");
+			return false;
+		}
+		if(f.estructura.value==""){
+			alert("Error debe marcar una estructura");
+			return false;
+		}		
 		menu();
 		event.preventDefault();
+	}
+	
+	function validarNumeros(nodo,campo){
+		if(isNaN(nodo.value) || nodo.value<1 || nodo.value>10)
+			nodo.setCustomValidity("Error el campo "+campo+" debe ser numérico");
+		else
+			nodo.setCustomValidity("");
 	}
 	
 	window.onload=function(){
 		let f=document.formu;
 		f.onsubmit=validar;
+		
+		f.tabla.oninput=function(){
+			validarNumeros(this,"tabla");
+		};
+		f.cantidad.addEventListener("blur",function(){
+									validarNumeros(this,"cantidad");
+									});
 	}
 	
 	
