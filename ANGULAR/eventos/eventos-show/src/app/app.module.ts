e48866b085c084760f45493867465a2d1ce95265
Pablo Garramone <pablo@iessanvicente.com>
2023-03-17 10:46:39 +0100

    Tema 2

--- ANGULAR/eventos/eventos-show/src/app/app.module.ts
+++ ANGULAR/eventos/eventos-show/src/app/app.module.ts
@@ -1,16 +1,20 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { EventoShowComponent } from './evento-show/evento-show.component';
+import { EventoFiltroPipe } from './pipes/evento-filtro.pipe';
 
 @NgModule({
   declarations: [
     AppComponent,
-    EventoShowComponent
+    EventoShowComponent,
+    EventoFiltroPipe
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- ANGULAR/eventos/eventos-show/src/app/evento-show/evento-show.component.ts
+++ ANGULAR/eventos/eventos-show/src/app/evento-show/evento-show.component.ts
@@ -8,30 +8,71 @@ import { IEvento } from '../interfaces/i-evento';
 })
 export class EventoShowComponent {
   alturaImagen=200;
+  search="";
+
+  newEvent:IEvento={
+    title:"",
+    description:"",
+    image:"",
+    price:0,
+    date:new Date("")
+  };
 
   eventos: IEvento[] = [
     {
-      title: "Evento 1",
+      title: "Evento 1 Verano",
       image: 'assets/evento1.jpg',
-      date: '2016-10-03',
-      description: "Descripción del Evento 1",
+      date: new Date('2016-10-03'),
+      description: "Un evento espectacular",
       price: 100
     },
     {
-      title: "Evento 2",
+      title: "Evento 2 Primavera",
       image: 'assets/evento2.jpg',
-      date: '2021-10-03',
-      description: "Descripción del Evento 2",
+      date: new Date('2021-10-03'),
+      description: "Un evento de música",
       price: 70
     },
     {
-      title: "Evento 3",
+      title: "Evento 3 Verano",
       image: 'assets/acdc.jpg',
-      date: '2022-10-03',
-      description: "Descripción del Evento 3",
+      date: new Date('2022-10-03'),
+      description: "Sin descripción",
       price: 30
     }
   ];
 
+  orderDate(){
+    this.search="";
+    this.eventos.sort((evento1,evento2)=>evento1.date.getTime()-evento2.date.getTime());
+  }
+
+  orderPrice(){
+    this.search="";
+    //this.eventos.sort((evento1,evento2)=>evento1.price<evento2.price?-1:1);
+    this.eventos.sort((evento1,evento2)=>evento1.price-evento2.price);
+  }
+
+  addEvent(){
+    console.log("Insertar Evento en el Array");
+  }
+
+  changeImage(fileInput: HTMLInputElement) {
+
+    if (!fileInput.files || fileInput.files.length === 0) {
+     return;
+    }
+
+    const reader: FileReader = new FileReader();
+    reader.readAsDataURL(fileInput.files[0]);
+    reader.addEventListener('loadend', (e) => {
+      /*if(reader.result!=null)
+        this.newEvent.image = reader.result.toString();
+      else
+      this.newEvent.image = "";*/
+      this.newEvent.image = reader.result as string;
+    });
+
+   }
 
 }
